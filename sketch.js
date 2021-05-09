250af5402aa63aa315cea4bbc79096a4c40e7086
LivvyGrace <78546536+LivvyGrace@users.noreply.github.com>
2021-05-09 15:33:08 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ gardenImage = loadImage("garden.png");
 mouseImg1 = loadAnimation ("mouse1.png");
 catImg1 = loadAnimation("cat1.png");
 catImg2 = loadAnimation("cat2.png","cat3.png");
-mouseImg2 = loadAnimation("mouse2.png","mouse3.png","mouse4.png");
+mouseImg2 = loadAnimation("mouse2.png","mouse3.png");
 
 
 }
@@ -21,10 +21,10 @@ function setup(){
     
 
 garden = createSprite(160,180);
-garden.addImage("garden.png", gardenImage);
+garden.addImage(gardenImage);
 
 cat = createSprite(200,20);
-cat.addImage("cat1.png", catImg1);
+cat.addAnimation("catsleeping", catImg1);
 
 }
 
